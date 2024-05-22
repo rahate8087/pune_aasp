071dda426d6aae3f8cafe4072dec1e340273b6dd
rahate8087 <akashrahate0@gmail.com>
2024-05-22 20:54:02 +0530

    deleting the files

--- README.md
+++
@@ -1,2 +0,0 @@
-# pune_aasp
-This is where you can write a long description for your project   non useable 
--- eda.py
+++
@@ -1,7 +0,0 @@
-print('Exploratory Data Analysis')
-
-print('Hello World!!!')
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
